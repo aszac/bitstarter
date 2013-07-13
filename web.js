0627d67046160c61232835c04009c07d8b2c4f70
Agnieszka Szachewicz <aszac@wp.pl>
2013-07-13 17:31:05 +0000

    Add HTML text

--- web.js
+++ web.js
@@ -5,10 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
     var fileContents = fs.readFileSync("index.html", 'utf8').split('\n');
-    var size = fileContents.length;
-    var buffer = new Buffer(size);
-    buffer.toString('utf8');
-    response.send(buffer.toString('utf8'));
+    response.send(fileContents.toString('utf8'));
     });
 
 var port = process.env.PORT || 5000;
