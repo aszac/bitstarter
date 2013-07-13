97837cfda6c47f028bfeca86d253f1857926791b
Agnieszka Szachewicz <aszac@wp.pl>
2013-07-13 13:53:05 +0000

    Read index.html in web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var fileContents = fs.readFile(index.html);
+    var fileContents = fs.readFile('index.html');
     var buffer = new Buffer(fileContents);
     buffer.toString('utf-8');
 });
