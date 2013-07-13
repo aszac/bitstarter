b2ad311b45508f8aa53ba137297f00249a3c0d25
Agnieszka Szachewicz <aszac@wp.pl>
2013-07-13 14:42:43 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -4,9 +4,10 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var fileContents = fs.readFileSync('index.html', 'utf8');
-    var buffer = new Buffer(fileContents);
+    var fileContents = fs.readFileSync('index.html', 'utf8').split('\n');
+    var buffer = new Buffer(fileContents[0].toString(), 'utf8');
     buffer.toString('utf8');
+    response.send(buffer.toString('utf8');
 });
 
 var port = process.env.PORT || 5000;
