5999ace11ec54c41171cd753f77cf2983e7eccfc
Agnieszka Szachewicz <aszac@wp.pl>
2013-07-13 17:34:07 +0000

    Add HTML text

--- web.js
+++ web.js
@@ -1,14 +1,18 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+readfile = "index.html";
+var data = fs.readFileSync(readfile);
+
+buf = new Buffer(data);
+ 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var fileContents = fs.readFileSync("index.html", 'utf8').split('\n');
-    response.send(fileContents.toString('utf8'));
-    });
+  response.send(buf.toString('utf8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
