16fb6852adbf25f0913ab494d93027973b046e2b
Agnieszka Szachewicz <aszac@wp.pl>
2013-07-13 21:25:30 +0000

    Update grader

--- grader.js
+++ grader.js
@@ -6,24 +6,27 @@ and basic DOM parsing.
 
 References:
 
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
++ cheerio
+- https://github.com/MatthewMueller/cheerio
+- http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+- http://maxogden.com/scraping-with-node.html
 
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
++ commander.js
+- https://github.com/visionmedia/commander.js
+- http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
++ JSON
+- http://en.wikipedia.org/wiki/JSON
+- https://developer.mozilla.org/en-US/docs/JSON
+- https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var util = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,14 +64,34 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkTheFile = function(url, file, checks){
+    if(url == null) {
+var checkJson = checkHtmlFile(file, checks);
+var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson);
+    } else {
+rest.get(url).on('complete', function(result, response){
+if(result instanceof Error){
+console.error('Error: ' + util.format(response.message));
+}else{
+fs.writeFileSync('tmp', result);
+var checkJson = checkHtmlFile('tmp', checks);
+var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson);
+}
+});
+     }
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-f, --file <html_file>', 'Path to index.html', HTMLFILE_DEFAULT)
+        .option('-u, --url <url>, Path to url')
+.parse(process.argv);
+
+    checkTheFile(program.url, program.file, program.checks);
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
